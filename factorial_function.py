667c735226acf397cabc00a57a114d0031b3b720
Jeremy Smith <jeremysmith244@gmail.com>
2021-12-11 21:03:04 -0800

    update ex with logging

--- factorial_function.py
+++ factorial_function.py
@@ -1,6 +1,22 @@
+'''
+Factorial module
+'''
+
+import logging
+
+logging.basicConfig(
+    filename='./logging.log'
+)
+
 def factorial(num):
+    '''Returns factorial of integer numbers'''
+
     result = 1
     if num < 0:
+        logging.error('ERROR: Negative numbers are invalid!')
+        raise ValueError
+    if not isinstance(num, int):
+        logging.error('ERROR: Only integers are valid!')
         raise ValueError
     while num > 0:
         result *= num
